d825931bafd5d45a9fa26d1973eebed018e82bc3
Josphat Mwangi <josphatwanjiruw@gmail.com>
2022-03-11 13:50:46 -0800

    printing hexidecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,23 @@
-#include <stdio.h>                                                                                                            
-                                                                                                                              
-/**                                                                                                                           
- * main - Entry point                                                                                                         
- * Return:0                                                                                                                   
- */                                                                                                                           
-int main(void)                                                                                                                
-{                                                                                                                             
-        int i, y;
-	for (i = 0; i < 10; i++)
-	{                                                                                                                     
-                putchar (i + '0');                                                                                            
-        }                                                                                                                     
-        for (y = 'a'; y <= 'f'; y++)                                                                                          
-        {                                                                                                                     
-                putchar (y);                                                                                                  
-        }                                                                                                                     
-                                                                                                                              
-putchar('\n');                                                                                                                
-return (0);                                                                                                                   
-}            
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Description:prints numbers of base16 in lowercase
+ *
+ *Return: Always zero
+ */
+int main(void)
+{
+	int dig, letterhex, n;
+
+	for (dig = 48, n = 0; n < 10; dig++, n++)
+	{
+		putchar(dig);
+	}
+	for (letterhex = 97, n = 1; n < 7; letterhex++, n++)
+	{
+		putchar(letterhex);
+	}
+	putchar(10);
+	return (0);
+}
