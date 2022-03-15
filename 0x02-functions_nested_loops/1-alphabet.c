9b17214a28e9a5c6e6df609d888f114be80e3dd0
Josphat Mwangi <josphatwanjiruw@gmail.com>
2022-03-15 15:34:44 -0700

    change format to betty format

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
-#include "main.h"                                                                                        
-                                                                                                         
-/**                                                                                                      
- * print_alphabet - Check description                                                                    
- * Description: It prints the alphabet in lowercase followed by a new line                               
- * Return: Nothing.                                                                                      
- */ 
+#include "main.h"
 
+/**
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase followed by a new line
+ * Return: Nothing.
+ */
 void print_alphabet(void)
 {
 	char i;
