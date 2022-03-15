47a41c69f5af693c746055e6a2e8e36a8aeeb40b
Josphat Mwangi <josphatwanjiruw@gmail.com>
2022-03-15 16:27:38 -0700

    time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,60 +1,46 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- *print_times_table - prints timestable from n
- *
- *@n:input
- *
- *Return: void
- *
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
-	int result;
+	int i, j, k;
 
-	if (n <= 15 && n > 0)
+	if (n >= 0 && n <= 15)
 	{
-		int i;
-
 		for (i = 0; i <= n; i++)
 		{
-			int j;
-
 			for (j = 0; j <= n; j++)
 			{
-				result = i * j;
+				k = j * i;
 				if (j == 0)
 				{
-					_putchar('0' + result);
-				}
-				else if (result >= 10)
-				{
-					_putchar(' ');
-					_putchar('0' + (result / 10));
-					_putchar('0' + (result % 10));
-				}
-				else if (result >= 100)
-				{
-					_putchar(' ');
-					_putchar('0' + (result / 100));
-					_putchar('0' + ((result / 10) % 10));
-					_putchar('0' + (result % 10));
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
-				}
-				if (j != n)
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
 		}
 	}
-	else
-	{
-		_putchar('\n');
-	}
-}
